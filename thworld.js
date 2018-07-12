7966eface5293f1065da774e4d240a073a08fe5b
kiyon <kiyonlin@163.com>
2018-07-12 09:43:53 +0800

    modify node thworld

--- thworld.js
+++ thworld.js
@@ -1,49 +1,70 @@
-const puppeteer = require('puppeteer');
-var log4js = require('log4js');
+const puppeteer = require("puppeteer");
+var log4js = require("log4js");
 var logger = log4js.getLogger();
-logger.level = 'debug';
+logger.level = "debug";
 
 (async () => {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const page = await browser.newPage({
+        slowMo: 100
+    });
 
-    await page.goto('http://www.thworld.net/auth/login');
+    logger.info("开始签到");
 
-    await page.type('#email', 'email');
-    await page.type('#passwd', 'passwd');
+    await page.goto("http://www.thworld.net/auth/login");
 
-    await page.click('#login');
+    await page.type("#email", "email");
+    await page.type("#passwd", "passwd");
+
+    logger.info("登录...");
+    await page.click("#login");
 
     // 等待页面登录到首页，可以看到'签到获取流量'
     await page.waitFor(6000);
 
+    logger.info("等待验证条加载...");
+
     try {
-        // await page.reload();
-        let content = await page.content();
-        // console.log(content);
+        // let iframe;
 
-        const reg = /<iframe src="(.*whitelabel=1)"/g;
-        let result = reg.exec(content);
-        // console.log('iframe地址:', result[1]);
+        // for (let child of page.mainFrame().childFrames()) {
+        //     if (child.url().includes("https://authedmine.com")) {
+        //         iframe = child;
+        //         break;
+        //     }
+        // }
+        // const verifyMe = await iframe.$("#verify-me");
+        // console.log(await verifyMe.click());
+        // dumpFrameTree(page.mainFrame(), "");
+        // await browser.close();
 
-        const iframePage = await browser.newPage();
+        // function dumpFrameTree(frame, indent) {
+        //     console.log(indent + frame.url());
+        //     for (let child of frame.childFrames()) dumpFrameTree(child, indent + "  ");
+        // }
+        const iframe = page.frames().find(f => {
+            return f.url().includes("https://authedmine.com");
+        });
 
-        await iframePage.goto(result[1]);
-        // let iframeContent = await iframePage.content();
+        console.log("iframe", iframe);
+        // const verifyMe = await iframe.$("#verify-me");
+        //         console.log(await verifyMe.text());
+        // if (!iframe) {
+        //     logger.info("已经签到");
+        //     await browser.close();
+        // }
 
-        logger.info('点击验证码...');
-        await iframePage.click('#verify-me');
+        // logger.info("点击验证条，等待20秒...");
 
-        logger.info('等待60秒...');
-        await page.waitFor(60000);
+        // await iframe.click("#verify-me");
 
-        // await page.reload();
+        // await page.waitFor(20000);
 
-        logger.info('开始签到...');
-        await page.click('#checkin');
-        logger.info('签到成功');
+        // logger.info("开始签到...");
+        // await page.click("#checkin");
+        // logger.info("签到成功");
     } catch (e) {
-        logger.error('签到失败:', e);
+        logger.error("签到失败:", e);
     }
 
     await browser.close();
